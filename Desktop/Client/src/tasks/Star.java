60b3b22eaa7fceaf18824ef512fafcacbdbc3a52
Carl <d13122842@mydit.ie>
2016-02-21 07:19:38 +0000

    Using control and work messages, in JSON format. Client using objects for messages.

---
+++ Desktop/Client/src/tasks/Star.java
@@ -0,0 +1,22 @@
+package tasks;
+
+public class Star {
+	int x;
+	int y;
+	int radius;
+	int annulus;
+	int dannulus;
+	int boxwidth;
+	int threshold;
+	
+	public Star(int x, int y, int radius, int annulus, int dannulus, int boxwidth, int threshold) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.annulus = annulus;
+		this.dannulus = dannulus;
+		this.boxwidth = boxwidth;
+		this.threshold = threshold;
+	}
+
+}
