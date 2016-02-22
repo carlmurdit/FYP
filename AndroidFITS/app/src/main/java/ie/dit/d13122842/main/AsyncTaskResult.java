2f0a3b4c5f6f1551889179971a0dd8fb87ea788d
Carl <d13122842@mydit.ie>
2016-02-22 03:14:01 +0000

    AsyncTaskResult now has a message whether successful or not. toString() works for both success and failure.

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/main/AsyncTaskResult.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/main/AsyncTaskResult.java
@@ -3,19 +3,26 @@ package ie.dit.d13122842.main;
 public class AsyncTaskResult<T> {
     private T result;
     private Exception error;
+    private String message;
 
     //constructor for success
-    public AsyncTaskResult(T result) {
+    public AsyncTaskResult(T result, String successMessage) {
         super();
         this.result = result;
+        this.error = null;
+        this.message = successMessage;
     }
 
     //constructor for failure
     public AsyncTaskResult(Exception error, String failMessage) {
         super();
-        if (error == null) error = new Exception(failMessage);
-        Exception e = new Exception(failMessage+". "+ error.getMessage(), error);
-        this.error = e;
+        this.result = null;
+        this.error = error;
+        this.message = failMessage;
+    }
+
+    public boolean isError() {
+        return (error != null);
     }
 
     public T getResult() {
@@ -24,6 +31,16 @@ public class AsyncTaskResult<T> {
 
     public Exception getError() {
         return error;
+    }
+
+    public String toString() {
+        if (error == null) {
+            return message;
+        } else if (message == null) {
+            return error.getMessage();
+        } else {
+            return this.message + ": " + this.error.getMessage();
+        }
     }
 
 }
