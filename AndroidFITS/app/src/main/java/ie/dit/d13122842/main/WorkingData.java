1319d91587234467047febfe1308ccbb225c18bc
Carl <d13122842@mydit.ie>
2016-02-23 04:21:25 +0000

    Plane # now being displayed when printing out values

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/main/WorkingData.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/main/WorkingData.java
@@ -16,13 +16,13 @@ public class WorkingData {
         result = new double[1][boxSize][boxSize];
     }
 
-    public static String toString(double[][][] pixelArray) {
+    public static String toString(double[][][] pixelArray, String planeLabel) {
 
         StringBuilder sb = new StringBuilder();
         for (int p = 0; p < pixelArray.length; p++) {
             for (int x = 0; x < pixelArray[0].length; x++) {
                 for (int y = 0; y < pixelArray[0].length; y++) {
-                    sb.append(String.format("p%d, x%d, y%d, %.10f\n", p, x, y, pixelArray[p][x][y]));
+                    sb.append(String.format("p%s, x%d, y%d, %.10f\n", planeLabel, x, y, pixelArray[p][x][y]));
                 }
             }
         }
