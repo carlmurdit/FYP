0dfaeefc8d56a70a2b608e02f508eba17e2c21e7
Carl <d13122842@mydit.ie>
2016-02-28 19:37:37 +0000

    UI output formatted into columns

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/main/WorkingData.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/main/WorkingData.java
@@ -22,7 +22,7 @@ public class WorkingData {
         for (int p = 0; p < pixelArray.length; p++) {
             for (int x = 0; x < pixelArray[0].length; x++) {
                 for (int y = 0; y < pixelArray[0].length; y++) {
-                    sb.append(String.format("p%s, x%d, y%d, %.10f\n", planeLabel, x, y, pixelArray[p][x][y]));
+                    sb.append(String.format("p%s, x%02d, y%02d, %16.10f\n", planeLabel, x, y, pixelArray[p][x][y]));
                 }
             }
         }
