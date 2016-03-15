ff68dd7df1a7791ea7b24185884be246db2b5e02
Carl <d13122842@mydit.ie>
2016-03-15 04:38:40 +0000

    Config class to keep paths etc.

---
+++ FITSAPIServer/src/ie/dit/d13122842/Config.java
@@ -0,0 +1,12 @@
+package ie.dit.d13122842;
+
+public class Config {
+	
+	public static final String FITSDIR = 
+			"/Users/carl/Documents/git/fyp/FITSAPIServer/WebContent/fitsdir/";
+	public static final String BINDIR = 
+			"/Users/carl/Documents/git/fyp/FITS_C/src/bin/";
+	public static final String RAWFITS_URL = 
+			"https://s3-eu-west-1.amazonaws.com/astronomydata/AstronomyData/compressedRAW/";
+	
+}
