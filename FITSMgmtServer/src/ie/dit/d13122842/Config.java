cb50c5e4f1e478c41f9ed834dd95e719d942679f
Carl <d13122842@mydit.ie>
2016-03-16 15:44:25 +0000

    Added API Server and Result Server

--- FITSMgmtServer/src/ie/dit/d13122842/Config.java
+++ FITSMgmtServer/src/ie/dit/d13122842/Config.java
@@ -2,6 +2,7 @@ package ie.dit.d13122842;
 
 public class Config {
 	
+	// Amazon Web Services
 	public static class AWS {
 		public static final String ACCESS_KEY_ID = "AKIAJQ4SHAXAUY7VLEGQ";
 		public static final String SECRET_ACCESS_KEY = "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";
@@ -10,14 +11,24 @@ public class Config {
 		public static final String SOURCE_BUCKET_PREFIX = "AstronomyData/compressedRAW/";
 	}
 	
-	public static class RabbitMQ {
+	// Rabbit Message Queue Broker
+	public static class MQ {
 		// public static final String HOST = "192.168.3.21";
-		public static final String HOST = "192.168.43.82";
+		public static final String HOST = "147.252.142.204";
 		public static final String USER = "test";
 		public static final String PASS = "test";
 		public static final String CONTROL_QUEUE = "control_queue";
 		public static final String WORK_QUEUE = "work_queue";
-		public static final String MANAGEMENT_UI = "http://192.168.3.21:15672"; // FYI
+		public static final String MANAGEMENT_UI = "http://147.252.142.204:15672"; // FYI
 	}
 	
+	// API Server
+	public static class API {
+		public static final String HOST = "147.252.142.202";
+	}
+	
+	// Result Server
+	public static class Result {
+		public static final String HOST = "147.252.142.202";
+	}
 }
