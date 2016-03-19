aacadc4de58f946e42a1f34d8ffdf8ee5c321d15
Carl <d13122842@mydit.ie>
2016-03-19 16:55:48 +0000

    New class Magnitude (not implemented)

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/main/Magnitude.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/main/Magnitude.java
@@ -1,7 +1,17 @@
 package ie.dit.d13122842.main;
 
-/**
- * Created by carl on 19/03/16.
- */
+import android.os.Handler;
+import android.util.Log;
+
 public class Magnitude {
+    private final Handler handler;
+
+    public Magnitude(Handler handler) {
+        this.handler = handler;
+    }
+
+    public void doWork() {
+        // todo
+        Log.d("fyp", "In Magnitude");
+    }
 }
