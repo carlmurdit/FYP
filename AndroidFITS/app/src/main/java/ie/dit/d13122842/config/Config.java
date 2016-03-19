75710c01652cca136dfd34af02f2c7f8e59564e5
Carl <d13122842@mydit.ie>
2016-03-19 16:48:35 +0000

    QUEUE_URI and QUEUE_NAME_CTL are the only MQ info the phone should have.

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/config/Config.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/config/Config.java
@@ -3,12 +3,9 @@ package ie.dit.d13122842.config;
 public class Config {
 
     public static class MQ {
-        // public static final String QUEUE_URI = "amqp://test:test@192.168.3.21:5672";
-        public static final String QUEUE_URI = "amqp://test:test@192.168.43.82:5672";
-        public static final String MANAGEMENT_UI = "http://192.168.3.21:15672"; // FYI
+        // Management UI: http://192.168.3.21:15672
+        public static final String QUEUE_URI = "amqp://test:test@192.168.3.21:5672";
         public static final String QUEUE_NAME_CTL = "control_queue";
-        public static final String QUEUE_NAME_WRK = "work_queue";
-        public static final String QUEUE_NAME_RLT = "result_queue";
     }
 
 
