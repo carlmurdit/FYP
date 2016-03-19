34f5829acb80986bda57732a9347872683958ed6
Carl <d13122842@mydit.ie>
2016-03-19 16:53:23 +0000

    History no longer saves all values, just a running count and average

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/main/History.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/main/History.java
@@ -1,42 +1,28 @@
 package ie.dit.d13122842.main;
 
-import android.util.Log;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class History {
 
-    private static Map<String, Long> units = new HashMap<String, Long>();;
-
-    public static Map<String, Long> getUnits() {
-        return units;
-    }
+    private static int unitCount = 0;
+    private static int averageTime = 0;
 
     public static int getUnitCount() {
-        return units.size();
+        return unitCount;
     }
 
-    public static Long getAvgTimePerUnit() throws Exception {
-        if (units.size()==0) {
+    public static int getAverageTime() throws Exception {
+        if (unitCount == 0)
             throw new Exception("Attempt to get average time when no units have been timed.");
-        }
-        Long timeSum = new Long(0);
-        for (Long time : units.values()) {
-            timeSum += time;
-        }
-        return timeSum / units.size();
+        return averageTime;
     }
 
-    public static void reset() {
-        units.clear();
+    public static void insert(int value) {
+        averageTime = (unitCount * averageTime + value) / (unitCount + 1);
+        unitCount++;
     }
 
-    public static void record(String key, Long time) {
-        units.put(key, time);
-        for (Map.Entry<String, Long> unit : units.entrySet()) {
-            Log.d("fyp", "Time for "+unit.getKey()+": "+unit.getValue());
-        }
+    public synchronized static void reset() {
+        unitCount = 0;
+        averageTime = 0;
     }
 
 }
