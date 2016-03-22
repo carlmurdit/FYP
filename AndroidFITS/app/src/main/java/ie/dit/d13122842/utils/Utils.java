79b4946979b7cc94ed849e0beafb394055b39228
Carl <d13122842@mydit.ie>
2016-03-22 07:05:30 +0000

    Deleted version of UI for appending to listview.

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/utils/Utils.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/utils/Utils.java
@@ -7,16 +7,7 @@ import android.os.Message;
 import ie.dit.d13122842.main.Enums;
 
 public class Utils {
-
-    public static void tellUI(Handler handler, String msg) {
-        // send a message to another thread via a Handler
-        Message message = handler.obtainMessage();
-        Bundle bundle = new Bundle();
-        bundle.putString("msg", msg);
-        message.setData(bundle);
-        handler.sendMessage(message);
-    }
-
+    
     public static void tellUI(Handler handler, Enums.UITarget target, String str) {
         // send a message to another thread via a Handler
         Message message = handler.obtainMessage();
