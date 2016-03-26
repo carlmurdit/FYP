f2cf2f1d2bb59f95c6203109a1434e31cf0c940d
Carl <d13122842@mydit.ie>
2016-03-26 08:06:01 +0000

    CID renamed to actID (as term Control no longer used). Using lwr case for all vars now.

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/messages/ControlMessage.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/messages/ControlMessage.java
@@ -6,17 +6,17 @@ import org.json.simple.parser.ParseException;
 
 public class ControlMessage {
 
-    private String CID;
-    private String Desc;
-    private String Work_Q_URL;
-    private String Work_Q_Name;
-    private String Result_Q_URL;
-    private String Result_Q_Name;
-    private String API_Server_URL;
-    private String Result_Server_URL;
-    private String Flat_Filename;
-    private String Bias_Filename;
-    private String Config_Filename;
+    private String actID;
+    private String desc;
+    private String work_Q_URL;
+    private String work_Q_Name;
+    private String result_Q_URL;
+    private String result_Q_Name;
+    private String api_Server_URL;
+    private String result_Server_URL;
+    private String flat_Filename;
+    private String bias_Filename;
+    private String config_Filename;
 
     // Constructor to take the JSON in the MQ message
     public ControlMessage (String json) throws Exception {
@@ -33,7 +33,7 @@ public class ControlMessage {
         //System.out.println(" -> JSON object: '" + obj.toJSONString() + "'");
         try {
             init(
-                    obj.get("CID"), obj.get("Desc"), obj.get("Work Q URL"), obj.get("Work Q Name"),
+                    obj.get("ActID"), obj.get("Desc"), obj.get("Work Q URL"), obj.get("Work Q Name"),
                     obj.get("Result Q URL"), obj.get("Result Q Name"), obj.get("API Server URL"),
                     obj.get("Result Server URL"),obj.get("Flat Filename"), obj.get("Bias Filename"),
                     obj.get("Config Filename"));
@@ -42,120 +42,120 @@ public class ControlMessage {
         }
     }
 
-    private void init(Object CID, Object Desc, Object Work_Q_URL,
+    private void init(Object actID, Object Desc, Object Work_Q_URL,
                           Object Work_Q_Name, Object Result_Q_URL, Object Result_Q_Name,
                           Object API_Server_URL, Object Result_Server_URL, Object Flat_Filename, Object Bias_Filename,
                           Object Config_Filename) {
 
-        this.CID = (String) CID;
-        this.Desc = (String) Desc;
-        this.Work_Q_URL = (String) Work_Q_URL;
-        this.Work_Q_Name = (String) Work_Q_Name;
-        this.Result_Q_URL = (String) Result_Q_URL;
-        this.Result_Q_Name = (String) Result_Q_Name;
-        this.API_Server_URL = (String) API_Server_URL;
-        this.Result_Server_URL = (String) Result_Server_URL;
-        this.Flat_Filename = (String) Flat_Filename;
-        this.Bias_Filename = (String) Bias_Filename;
-        this.Config_Filename = (String) Config_Filename;
+        this.actID = (String) actID;
+        this.desc = (String) Desc;
+        this.work_Q_URL = (String) Work_Q_URL;
+        this.work_Q_Name = (String) Work_Q_Name;
+        this.result_Q_URL = (String) Result_Q_URL;
+        this.result_Q_Name = (String) Result_Q_Name;
+        this.api_Server_URL = (String) API_Server_URL;
+        this.result_Server_URL = (String) Result_Server_URL;
+        this.flat_Filename = (String) Flat_Filename;
+        this.bias_Filename = (String) Bias_Filename;
+        this.config_Filename = (String) Config_Filename;
     }
 
-    public String getCID() {
-        return CID;
+    public String getActID() {
+        return actID;
     }
 
-    public void setCID(String cID) {
-        CID = cID;
+    public void setCID(String actID) {
+        this.actID = actID;
     }
 
     public String getDesc() {
-        return Desc;
+        return desc;
     }
 
     public void setDesc(String desc) {
-        Desc = desc;
+        this.desc = desc;
     }
 
     public String getWork_Q_URL() {
-        return Work_Q_URL;
+        return work_Q_URL;
     }
 
     public void setWork_Q_URL(String work_Q_URL) {
-        Work_Q_URL = work_Q_URL;
+        this.work_Q_URL = work_Q_URL;
     }
 
     public String getWork_Q_Name() {
-        return Work_Q_Name;
+        return work_Q_Name;
     }
 
     public void setWork_Q_Name(String work_Q_Name) {
-        Work_Q_Name = work_Q_Name;
+        this.work_Q_Name = work_Q_Name;
     }
 
     public String getResult_Q_URL() {
-        return Result_Q_URL;
+        return result_Q_URL;
     }
 
     public void setResult_Q_URL(String result_Q_URL) {
-        Result_Q_URL = result_Q_URL;
+        this.result_Q_URL = result_Q_URL;
     }
 
     public String getResult_Q_Name() {
-        return Result_Q_Name;
+        return result_Q_Name;
     }
 
     public void setResult_Q_Name(String result_Q_Name) {
-        Result_Q_Name = result_Q_Name;
+        this.result_Q_Name = result_Q_Name;
     }
 
     public String getAPI_Server_URL() {
-        return API_Server_URL;
+        return api_Server_URL;
     }
 
     public void setAPI_Server_URL(String aPI_Server_URL) {
-        API_Server_URL = aPI_Server_URL;
+        api_Server_URL = aPI_Server_URL;
     }
 
     public String getResult_Server_URL() {
-        return Result_Server_URL;
+        return result_Server_URL;
     }
 
     public void setResult_Server_URL(String result_Server_URL) {
-        Result_Server_URL = result_Server_URL;
+        result_Server_URL = result_Server_URL;
     }
 
     public String getFlat_Filename() {
-        return Flat_Filename;
+        return flat_Filename;
     }
 
     public void setFlat_Filename(String flat_Filename) {
-        Flat_Filename = flat_Filename;
+        this.flat_Filename = flat_Filename;
     }
 
     public String getBias_Filename() {
-        return Bias_Filename;
+        return bias_Filename;
     }
 
     public void setBias_Filename(String bias_Filename) {
-        Bias_Filename = bias_Filename;
+        this.bias_Filename = bias_Filename;
     }
 
     public String getConfig_Filename() {
-        return Config_Filename;
+        return config_Filename;
     }
 
     public void setConfig_Filename(String config_Filename) {
-        Config_Filename = config_Filename;
+        this.config_Filename = config_Filename;
     }
 
     public String toString() {
-        return String.format("CID:%s\nDesc:%s\nWork_Q_URL:%s\n"
+        return String.format("actID:%s\ndesc:%s\nWork_Q_URL:%s\n"
                         + "Work_Q_Name:%s\nResult_Q_URL:%s\nResult_Q_Name:%s\n"
                         + "API_Server_URL:%s\nFlat_Filename:%s\nBias_Filename:%s\n"
                         + "Config_Filename:%s",
-                CID, Desc, Work_Q_URL, Work_Q_Name, Result_Q_URL, Result_Q_Name,
-                API_Server_URL, Flat_Filename, Bias_Filename,
-                Config_Filename);
+                actID, desc, work_Q_URL, work_Q_Name, result_Q_URL, result_Q_Name,
+                api_Server_URL, flat_Filename, bias_Filename,
+                config_Filename);
     }
 
 }
