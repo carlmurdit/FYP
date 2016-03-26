d654d5cbb558701f6ce57d1d06a6f5ac5dede330
Carl <d13122842@mydit.ie>
2016-03-26 08:08:41 +0000

    Unused code deleted

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/main/MainActivity.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/main/MainActivity.java
@@ -199,7 +199,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //    publishThread.interrupt();
         subscribeThread.interrupt();
     }
 
