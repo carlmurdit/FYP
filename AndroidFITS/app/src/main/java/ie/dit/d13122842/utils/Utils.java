d420b03091ea7ea468fcb6991cfee2ee8b220bd6
Carl <d13122842@mydit.ie>
2016-03-30 05:27:26 +0100

    Moved longLogV from ControlClient

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/utils/Utils.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/utils/Utils.java
@@ -3,6 +3,7 @@ package ie.dit.d13122842.utils;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 
 import ie.dit.d13122842.main.Enums;
 
@@ -35,6 +36,17 @@ public class Utils {
         bundle.putString("tgt", target.name());
         message.setData(bundle);
         handler.sendMessage(message);
+    }
+
+    public static void longLogV(String tag, String str, boolean enabled) {
+        // Split debug messages into multiple smaller ones
+        if(!enabled) return; // disable
+        // avoid max length of logcat messages
+        if(str.length() > 4000) {
+            Log.v(tag, str.substring(0, 4000));
+            longLogV(tag, str.substring(4000), enabled);
+        } else
+            Log.v(tag, str);
     }
 
 }
