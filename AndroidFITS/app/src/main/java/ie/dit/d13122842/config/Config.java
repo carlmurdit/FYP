c351618b1246be8becbe2b47b00e855b14037b2c
Carl <d13122842@mydit.ie>
2016-03-30 03:49:11 +0100

    using "Activation" instead of "Control"

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/config/Config.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/config/Config.java
@@ -5,7 +5,7 @@ public class Config {
     public static class MQ {
         // Management UI: http://192.168.3.21:15672
         public static final String QUEUE_URI = "amqp://test:test@192.168.3.21:5672";
-        public static final String QUEUE_NAME_CTL = "control_queue";
+        public static final String QUEUE_NAME_ACT = "activation_queue";
     }
 
 
