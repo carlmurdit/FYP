2b561793df15f515969c1dbe3391bb792dbb5a71
Carl <d13122842@mydit.ie>
2016-03-30 05:39:13 +0100

    stars is module-level so all methods can see it. CTL (control) prefix changed to ACT (Activation). No longer using a QueueingConsumer for Work messages - using basicGet() instead to avoid  a Work Messages being consumed before its Activation message.  Removed longLogV to Utils class.

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/main/ControlClient.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/main/ControlClient.java
@@ -7,6 +7,7 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.GetResponse;
 import com.rabbitmq.client.QueueingConsumer;
 import com.rabbitmq.client.ShutdownSignalException;
 
@@ -15,7 +16,6 @@ import java.util.ArrayList;
 
 import ie.dit.d13122842.config.Config;
 import ie.dit.d13122842.messages.ControlMessage;
-import ie.dit.d13122842.messages.WorkMessage;
 import ie.dit.d13122842.posting.FormPoster;
 import ie.dit.d13122842.utils.Timer;
 import ie.dit.d13122842.utils.Utils;
@@ -24,9 +24,12 @@ public class ControlClient implements Runnable {
 
     private final boolean AUTOACK_ON = true;
     private final boolean AUTOACK_OFF = false;
+    boolean MULTIPLE_OFF = false;
+    boolean RE_QUEUE_ON = true;
     private final Handler handler;
     private final ConnectionFactory factory;
     private final String androidId;
+    private ArrayList<Star> stars = null;  // Contents of the Config File
 
     public ControlClient(Handler handler, ConnectionFactory factory, String androidId) {
         this.handler = handler;
@@ -37,111 +40,104 @@ public class ControlClient implements Runnable {
     @Override
     public void run() {
         String sMsg; // used for debug and info messages
-        int ctli = 0;
-        int wrki = 0;
 
         while (true) {
             try {
 
-                ctli++;
-
-                Utils.tellUI(handler, Enums.UITarget.CTL_HEAD, "No work");
-                Utils.tellUI(handler, Enums.UITarget.CTL_STATUS, "Connecting...");
+                Utils.tellUI(handler, Enums.UITarget.ACT_HEAD, "No work");
+                Utils.tellUI(handler, Enums.UITarget.ACT_STATUS, "Connecting...");
                 Utils.tellUI(handler, Enums.UITarget.ERROR, "");
 
                 // create a connection
                 Connection connection = factory.newConnection();
 
                 // Create a channel for Control messages
-                final Channel channelCTL = connection.createChannel();
-                channelCTL.queueDeclare(Config.MQ.QUEUE_NAME_CTL, true, false, false, null);
-                channelCTL.basicQos(1); // prefetch count
+                final Channel channelACT = connection.createChannel();
+                channelACT.queueDeclare(Config.MQ.QUEUE_NAME_ACT, true, false, false, null);
+                channelACT.basicQos(1); // prefetch count
 
-                // Subscribe to receive messages from the Control Queue
-                QueueingConsumer consumer = new QueueingConsumer(channelCTL);
-                channelCTL.basicConsume(Config.MQ.QUEUE_NAME_CTL, AUTOACK_ON, consumer);
-
-                // Get a control message
-
-                // while (true) {
-                sMsg="Waiting for instructions...";
-                Log.d("fyp", sMsg);
-                Utils.tellUI(handler, Enums.UITarget.CTL_STATUS, sMsg);
-
-                // Block until next Control message received
-                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-
-                final String messageCTL = new String(delivery.getBody());
-                ControlMessage ctlMsg = new ControlMessage(messageCTL);
-                Log.d("fyp", "Control Message parsed:\n" + ctlMsg.toString() + "\n");
-                Utils.tellUI(handler, Enums.UITarget.CTL_HEAD, ctli+ "_" + ctlMsg.getDesc());
-
-                // log the messages we are receiving
-                Log.d("fyp", "CTL nextDelivery() was iteration "+ctli+" for " +ctlMsg.getDesc());
-
-                // Populate Stars with config data and flat/bias pixels
-                ArrayList<Star> stars;
-                try {
-                    stars = getStars(ctlMsg);
-                } catch (Exception e) {
-                    throw new Exception("Could not load config data. Cannot process work. "+e.getMessage(),e);
-                }
-
-                // create a channel for Work messages
-                final Channel channelWRK = connection.createChannel();
-                channelWRK.queueDeclare(ctlMsg.getWork_Q_Name(), true, false, false, null);
-                channelWRK.basicQos(1); // prefetch count
-
-                // Create the QueueingConsumer and have it consume from the queue
-                QueueingConsumer consumerWRK = new QueueingConsumer(channelWRK);
-                channelWRK.basicConsume(ctlMsg.getWork_Q_Name(), AUTOACK_OFF, consumerWRK);
-
-                // create a channel for Result messages
-                final Channel channelResult = connection.createChannel();
-                channelResult.queueDeclare(ctlMsg.getResult_Q_Name(), true, false, false, null);
-                // channelResult.basicQos(1); // prefetch count
-
-                Utils.tellUI(handler, Enums.UITarget.CTL_STATUS, "Active");
+                // Subscribe to receive messages from the Activation Queue
+                QueueingConsumer consumerACT = new QueueingConsumer(channelACT);
+                channelACT.basicConsume(Config.MQ.QUEUE_NAME_ACT, AUTOACK_OFF, consumerACT);
 
                 while (true) {
+                    // Loop after every Activation message
 
-                    Utils.tellUI(handler, Enums.UITarget.WRK_HEAD, "No Work");
-                    Utils.tellUI(handler, Enums.UITarget.WRK_STATUS_1, "Waiting for instructions...");
+                    sMsg = "Waiting for instructions...";
+                    Log.d("fyp", sMsg);
+                    Utils.tellUI(handler, Enums.UITarget.ACT_HEAD, "Idle");
+                    Utils.tellUI(handler, Enums.UITarget.ACT_STATUS, sMsg);
+                    Utils.tellUI(handler, Enums.UITarget.WRK_HEAD, "");
+                    Utils.tellUI(handler, Enums.UITarget.WRK_STATUS_1, "");
 
-                    // Wait for and process each WORK message
-                    QueueingConsumer.Delivery deliveryWRK = consumerWRK.nextDelivery();
-                    final String rawMessageWRK = new String(deliveryWRK.getBody());
+                    // Block until next Activation message received
+                    QueueingConsumer.Delivery deliveryACT = consumerACT.nextDelivery();
 
-                    Log.d("fyp", "RECEIVED WORK MESSAGE");
-                    // log the messages we are receiving
-                    wrki++;
-                    Log.e("fyp", "WRK nextDelivery() was iteration " + wrki + " for " +new WorkMessage(rawMessageWRK).getFilename());
+                    // Parse and display the Activity
+                    final String messageACT = new String(deliveryACT.getBody());
+                    ControlMessage actMsg = new ControlMessage(messageACT);
+                    Log.d("fyp", "Activation Message parsed:\n" + actMsg.toString() + "\n");
+                    Utils.tellUI(handler, Enums.UITarget.ACT_HEAD, actMsg.getDesc());
+                    Utils.tellUI(handler, Enums.UITarget.ACT_STATUS, "Initialising Activity...");
 
-                    // Start timing for this work unit (e.g. Fits File)
+                    // Download Config then get flat/bias if we don't have them
+                    try {
+                        stars = getStars(actMsg);
+                    } catch (Exception e) {
+                        stars = null;
+                        throw new Exception("Could not load config data. Cannot process work. " + e.getMessage(), e);
+                    }
+
+                    // create a channel for Work messages
+                    final Channel channelWRK = connection.createChannel();
+                    channelWRK.queueDeclare(actMsg.getWork_Q_Name(), true, false, false, null);
+                    channelWRK.basicQos(1); // prefetch count
+
+                    // create a channel for Result messages
+                    final Channel channelResult = connection.createChannel();
+                    channelResult.queueDeclare(actMsg.getResult_Q_Name(), true, false, false, null);
+
+                    Utils.tellUI(handler, Enums.UITarget.ACT_STATUS, "Active");
+                    GetResponse response = channelWRK.basicGet(actMsg.getWork_Q_Name(), AUTOACK_OFF);
+                    String rawMessageWRK;
+                    if (response == null) {
+                        // No message retrieved so re-queue the Activation Message
+                        Log.e("fyp", "No WRK Message Found!");
+                        channelACT.basicNack(deliveryACT.getEnvelope().getDeliveryTag(), MULTIPLE_OFF, RE_QUEUE_ON);
+                        Utils.tellUI(handler, Enums.UITarget.WRK_STATUS_1, "No Work Found for Activity");
+                        Thread.sleep(500); // ToDo: Remove if not an issue
+                        continue; // go get next CTL message
+                    } else {
+                        Log.d("fyp", "RECEIVED WORK MESSAGE");
+                        rawMessageWRK = new String(response.getBody());
+                    }
+
+                    // Start timing for this work unit (i.e. Fits File)
                     Timer timer = new Timer();
                     timer.start();
 
-                    if (ctlMsg.getActID().compareTo("1")==0) {
+                    if (actMsg.getActID().compareTo("1") == 0) {
                         // Activity is FITS Cleaning
                         Cleaner cleaner = new Cleaner(handler);
-                        cleaner.doWork(ctlMsg, stars, rawMessageWRK, channelResult, androidId);
-                    } else if (ctlMsg.getActID().compareTo("2")==0) {
+                        cleaner.doWork(actMsg, stars, rawMessageWRK, channelResult, androidId);
+                    } else if (actMsg.getActID().compareTo("2") == 0) {
                         // Activity is Magnitude Calculation
                         Magnitude magnitude = new Magnitude(handler);
                         magnitude.doWork();
                     } else {
-                        Log.d("fyp", "ctlMsg.getCID()="+ctlMsg.getActID());
+                        Log.d("fyp", "ctlMsg.getActID()=" + actMsg.getActID());
                     }
 
                     // tell MQ it can delete the message
-                    channelWRK.basicAck(deliveryWRK.getEnvelope().getDeliveryTag(), false);
+                    channelACT.basicAck(deliveryACT.getEnvelope().getDeliveryTag(), false);
+                    channelWRK.basicAck(response.getEnvelope().getDeliveryTag(), false);
 
                     // Update the History and its UI controls
                     History.insert(timer.stop());
                     Utils.tellUI(handler, Enums.UITarget.SUMMARY_1, String.format("%d", History.getUnitCount()));
                     Utils.tellUI(handler, Enums.UITarget.SUMMARY_2, String.format("%d ms.", History.getAverageTime()));
 
-                }
+                } // end while
 
 
             } catch (InterruptedException e) {
@@ -149,33 +145,37 @@ public class ControlClient implements Runnable {
                 Log.e("fyp", sMsg, e);
                 Utils.tellUI(handler, Enums.UITarget.RESETALL);
                 Utils.tellUI(handler, Enums.UITarget.ERROR, sMsg);
+                stars = null; // require re-download
                 break;
             } catch (ShutdownSignalException e) {
                 sMsg = "The connection was shut down while waiting for messages. " + e.getMessage();
                 Log.e("fyp", sMsg, e);
                 Utils.tellUI(handler, Enums.UITarget.ERROR, sMsg);
+                stars = null; // require re-download
                 break;
             } catch (ConsumerCancelledException e) {
                 sMsg = "The consumer was cancelled while waiting for messages. " + e.getMessage();
                 Log.e("fyp", sMsg, e);
                 Utils.tellUI(handler, Enums.UITarget.ERROR, sMsg);
+                stars = null; // require re-download
                 break;
             } catch (IOException e) {
                 sMsg = "IOException: " + e.getMessage();
                 Log.e("fyp", sMsg, e);
                 Utils.tellUI(handler, Enums.UITarget.ERROR, sMsg);
+                stars = null; // require re-download
                 break;
             } catch (Exception e) {
                 sMsg = "Connection broken, retrying: \n" + e.getClass().getName() + ", " + e.getMessage();
                 Log.e("fyp", sMsg, e);
                 Utils.tellUI(handler, Enums.UITarget.ERROR, sMsg);
+                stars = null; // require re-download
                 try {
                     Thread.sleep(4000); //sleep and then try again
                 } catch (InterruptedException ie) {
                     break;
                 }
             }
-
         }
     }
 
@@ -187,15 +187,15 @@ public class ControlClient implements Runnable {
         // Download the config (star list) file
         sMsg = "Requesting "+ctlMsg.getConfig_Filename()+" from " + ctlMsg.getAPI_Server_URL() + "...";
         Log.d("fyp", sMsg);
-        Utils.tellUI(handler, Enums.UITarget.CTL_HEAD, ctlMsg.getDesc());
-        Utils.tellUI(handler, Enums.UITarget.CTL_STATUS, "Downloading "+ctlMsg.getConfig_Filename()+"...");
+        Utils.tellUI(handler, Enums.UITarget.ACT_HEAD, ctlMsg.getDesc());
+        Utils.tellUI(handler, Enums.UITarget.ACT_STATUS, "Downloading "+ctlMsg.getConfig_Filename()+"...");
         FormPoster poster = new FormPoster(ctlMsg.getAPI_Server_URL());
         poster.add("action", "getfile");  // set POST variables
         poster.add("filename", ctlMsg.getConfig_Filename());
         String configContents = poster.post();
 
         // parse the contents into stars
-        final ArrayList<Star> stars = new ArrayList<>();
+        ArrayList<Star> stars = new ArrayList<>();
         String[] lines = configContents.split("\n"); //todo handle \r\n endings?
         if (lines.length==0)
             throw new Exception("The Config does not contain any stars: "+configContents);
@@ -216,6 +216,22 @@ public class ControlClient implements Runnable {
         }
         Log.d("fyp", sMsg);
 
+        // Compare previously downloaded Config (if found) to this download
+        if (this.stars != null && this.stars.size() == stars.size()) {
+            boolean differs = false;
+            for (int i=0; i<stars.size(); i++) {
+                Star star = stars.get(i);
+                Star oldStar = this.stars.get(i);
+                if (star.getBox().compareTo(oldStar.getBox()) != 0) {
+                    // difference found, so re-download flat and bias
+                    differs = true;
+                    break;
+                }
+            }
+            // all same so no need to re-download
+            if (!differs) return this.stars;
+        }
+
         // Update each Star with flat and bias boxes from the server
         for (int i=0; i<stars.size(); i++) {
             Star star = stars.get(i);
@@ -224,7 +240,7 @@ public class ControlClient implements Runnable {
             sMsg = String.format("GETTING FLAT, STAR %d...\nX %d, Y %d, Box width %d, %s from %s...",
                     i+1, star.getX(), star.getY(), star.getBoxwidth(), star.getBox(), ctlMsg.getFlat_Filename());
             Log.d("fyp", sMsg);
-            Utils.tellUI(handler, Enums.UITarget.CTL_STATUS, "Downloading "+ctlMsg.getFlat_Filename()+"...");
+            Utils.tellUI(handler, Enums.UITarget.ACT_STATUS, "Downloading "+ctlMsg.getFlat_Filename()+"...");
             poster = new FormPoster(ctlMsg.getAPI_Server_URL());
             poster.add("action", "getbox");  // add POST variables
             poster.add("box", star.getBox());
@@ -235,13 +251,13 @@ public class ControlClient implements Runnable {
             // populate star's Flat array from the returned data
             star.setFlatPixels(PixelBox.stringToArray(star.getBoxwidth(), flatResponse));
             Log.d("fyp", "FLAT RECEIVED.");
-            longLogv("fyp", PixelBox.arrayToString(star.getFlatPixels(), "-"));
+            Utils.longLogV("fyp", PixelBox.arrayToString(star.getFlatPixels(), "-"), false);
 
             // repeat for Bias
             sMsg = String.format("GETTING BIAS, STAR %d:\nX %d, Y %d, Box width %d, %s from %s...",
                     i+1, star.getX(), star.getY(), star.getBoxwidth(), star.getBox(), ctlMsg.getBias_Filename());
             Log.d("fyp", sMsg);
-            Utils.tellUI(handler, Enums.UITarget.CTL_STATUS, "Downloading "+ctlMsg.getBias_Filename()+"...");
+            Utils.tellUI(handler, Enums.UITarget.ACT_STATUS, "Downloading "+ctlMsg.getBias_Filename()+"...");
             poster = new FormPoster(ctlMsg.getAPI_Server_URL());
             poster.add("action", "getbox");
             poster.add("box", star.getBox());
@@ -252,22 +268,12 @@ public class ControlClient implements Runnable {
             // populate star's Bias array from the returned data
             star.setBiasPixels(PixelBox.stringToArray(star.getBoxwidth(), biasResponse));
             Log.d("fyp", "BIAS RECEIVED.");
-            longLogv("fyp", PixelBox.arrayToString(star.getBiasPixels(), "-"));
+            Utils.longLogV("fyp", PixelBox.arrayToString(star.getBiasPixels(), "-"), false);
 
         }
 
         return stars;
 
-    }
-
-    public void longLogv(String tag, String str) {
-        if(true) return; // disable
-        // avoid max length of logcat messages
-        if(str.length() > 4000) {
-            Log.v(tag, str.substring(0, 4000));
-            longLogv(tag, str.substring(4000));
-        } else
-            Log.v(tag, str);
     }
 
 }
