586dfc68264fd0b1c0644ca997134a98fffd5990
Carl <d13122842@mydit.ie>
2016-03-30 03:50:57 +0100

    Using ACT (Activation) instead of CTL (Control)

--- AndroidFITS/app/src/main/java/ie/dit/d13122842/main/Enums.java
+++ AndroidFITS/app/src/main/java/ie/dit/d13122842/main/Enums.java
@@ -2,8 +2,8 @@ package ie.dit.d13122842.main;
 
 public class Enums {
     public enum UITarget {
-        CTL_HEAD,           // "Waiting..." or "FITS Cleaning" etc.
-        CTL_STATUS,         // "Downloading flat...", "Ready" etc.
+        ACT_HEAD,           // "Waiting..." or "FITS Cleaning" etc.
+        ACT_STATUS,         // "Downloading flat...", "Ready" etc.
         WRK_HEAD,           // "No work", "Waiting...", "0000001.fits" etc
         WRK_PROGRESS_MAX,   // To send message to set Progress Bar max value
         WRK_PROGRESS_RESET, // To send message to reset Progress Bar value
