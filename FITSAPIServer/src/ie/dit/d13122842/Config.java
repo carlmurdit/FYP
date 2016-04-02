7e49d0fbc4c64ba2bfdeda66047d108a77729740
Carl <d13122842@mydit.ie>
2016-04-02 22:46:32 +0100

    BINDIR replaced with constants for fits_subset and funpack executables

--- FITSAPIServer/src/ie/dit/d13122842/Config.java
+++ FITSAPIServer/src/ie/dit/d13122842/Config.java
@@ -4,9 +4,12 @@ public class Config {
 	
 	public static final String FITSDIR = 
 			"/Users/carl/Documents/git/fyp/FITSAPIServer/WebContent/fitsdir/";
-	public static final String BINDIR = 
-			"/Users/carl/Documents/git/fyp/FITS_C/src/bin/";
 	public static final String RAWFITS_URL = 
 			"https://s3-eu-west-1.amazonaws.com/astronomydata/AstronomyData/compressedRAW/";
+	
+	public static class app {
+		public static final String FITS_SUBSET = "/Users/carl/Documents/git/fyp/FITS_C/src/bin/fits_subset";
+		public static final String FUNPACK = "/Users/carl/Documents/git/fyp/FITS_C/src/bin/funpack";
+	}
 	
 }
