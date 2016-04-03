905b1799638ddb86c6444f9501e2bbbc236b4eb6
Carl <d13122842@mydit.ie>
2016-04-03 21:30:10 +0100

    results and followingJob are part of a result message now.

--- FITSMgmtServer/src/ie/dit/d13122842/ResultMessage.java
+++ FITSMgmtServer/src/ie/dit/d13122842/ResultMessage.java
@@ -7,7 +7,7 @@ import org.json.simple.parser.ParseException;
 public class ResultMessage {
     private boolean success;
     private String activity;
-    private String filename;
+    private String sourceFileName;
     private int planes;
     private int starNumber;
     private String box;
@@ -15,17 +15,18 @@ public class ResultMessage {
     private String androidId;
     private String errorMessage;
     private String s3url; // e.g. https://s3-us-west-2.amazonaws.com/cleanedfits/cleaned/0000001_2.fits
+    private String results;
+    private String followingJob;
+    
+    private JSONObject obj;
     
     public ResultMessage(String jsonString) throws Exception {
     	
-    	System.out.println("ResultMessage... 1");
-    	
         // https://code.google.com/archive/p/json-simple
-        JSONParser parser = new JSONParser();
-        JSONObject obj;
+        JSONParser parser = new JSONParser();       
         try {
             obj = (JSONObject) parser.parse(jsonString);
-            System.out.println("ResultMessage jsonString: "+ obj.toJSONString() + "'");
+            System.out.println("ResultMessage jsonString: "+ obj.toJSONString());
         } catch (ParseException e) {
             throw new Exception("Error parsing JSON of the result message.\n" + e.getMessage() + "\n" + jsonString);
         }   
@@ -33,21 +34,23 @@ public class ResultMessage {
         try {        	
 	        this.success = (boolean) obj.get("success");
 	        this.activity = (String) obj.get("activity");
-	        this.filename = (String) obj.get("filename");
+	        this.sourceFileName = (String) obj.get("sourceFileName");
 	        this.planes = (int)(long) obj.get("planes");
 	        this.starNumber = (int)(long) obj.get("starNumber");
 	        this.box = (String) obj.get("box");
 	        this.processingTime = (long) obj.get("processingTime");
 	        this.androidId = (String) obj.get("androidId");
 	        this.errorMessage = (String) obj.get("errorMessage");
-	        this.s3url = "<a href=\"" +(String) obj.get("s3url")+"\">"+obj.get("s3url")+"</a>";	        
+	        this.s3url = "<a href=\"" +(String) obj.get("s3url")+"\">"+obj.get("s3url")+"</a>";	
+	        this.results = (String) obj.get("results");
+	        this.followingJob = (String) obj.get("followingJob");
 	    } catch (Exception e) {
 	        throw new Exception("Error creating ResultMessage from JSON.\n" + e.getMessage() + "\n" + jsonString);
 	    } 	
         
     }
 
-    public boolean isSuccess() {
+    public boolean getSuccess() {
         return success;
     }
 
@@ -55,8 +58,8 @@ public class ResultMessage {
         return activity;
     }
 
-    public String getFilename() {
-        return filename;
+    public String getSourceFileName() {
+        return sourceFileName;
     }
 
     public int getPlanes() {
@@ -83,15 +86,25 @@ public class ResultMessage {
         return errorMessage;
     }
 
-    public String getTooltip() {
-    	return getFilename()+", star "+getStarNumber();
-    }
-
 	public String getS3url() {
 		return s3url;
 	}
 
-	public void setS3url(String s3url) {
-		this.s3url = s3url;
+	public String getResults() {
+		return results;
 	}
+
+	public String getFollowingJob() {
+		return followingJob;
+	}
+	
+    public String getTooltip() {
+     	return getSourceFileName()+", star "+getStarNumber();
+    }
+    
+    @Override
+    public String toString() {
+    	return obj.toJSONString();
+    }
+
 }
