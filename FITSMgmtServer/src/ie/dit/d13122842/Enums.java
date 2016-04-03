6e9d992b03b8e88f456ddc0d4db339c820b68422
Carl <d13122842@mydit.ie>
2016-04-03 13:47:57 +0100

    New class for enums

---
+++ FITSMgmtServer/src/ie/dit/d13122842/Enums.java
@@ -0,0 +1,15 @@
+package ie.dit.d13122842;
+
+public class Enums {
+	
+	public enum FITS_Type {
+        RAW,
+        CLEAN
+	}
+	
+    public static class Activities {
+        public static final String CLEANING = "1";
+        public static final String MAGNITUDE = "2";
+    }
+    
+}
